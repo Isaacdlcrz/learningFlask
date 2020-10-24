aa430c9694e0f24b7b20aa4ab67a2810641cf728
Kenshex7 <hector12x13@gmail.com>
2020-10-24 12:20:15 -0500

    database uri
    Signed-off-by: Kenshex7 <hector12x13@gmail.com>

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymsql://root:Di322$25@127.0.0.1"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Di322$25@127.0.0.1:3306/sharePlay"
 
 class ProductionConfig(Config):
     DEBUG = False
