00431d85bf85b40c2f2125ad1b04d15e7eda9f08
Isaacdlcrz <62781353+Isaacdlcrz@users.noreply.github.com>
2020-10-28 09:37:12 -0600

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Di322$25@127.0.0.1:3306/sharePlay"
+    SQLALCHEMY_DATABASE_URI = ""
 
 class ProductionConfig(Config):
     DEBUG = False
